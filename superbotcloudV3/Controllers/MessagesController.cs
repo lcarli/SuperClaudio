1392875f8f72e44195ac77f8f83bb24d2760ef0e
Lucas <lucas.decarli@gmail.com>
2016-07-29 09:32:19 -0700

    Principal método de consulta aos arquivos

--- superbotcloudV3/Controllers/MessagesController.cs
+++ superbotcloudV3/Controllers/MessagesController.cs
@@ -7,12 +7,16 @@ using System.Web.Http;
 using System.Web.Http.Description;
 using Microsoft.Bot.Connector;
 using Newtonsoft.Json;
+using System.IO;
+using System.Web;
+using System.Collections.Generic;
 
 namespace superbotcloudV3
 {
     [BotAuthentication]
     public class MessagesController : ApiController
     {
+        Random random = new Random();
         /// <summary>
         /// POST: api/Messages
         /// Receive a message from a user and reply to it
@@ -22,11 +26,25 @@ namespace superbotcloudV3
             if (activity.Type == ActivityTypes.Message)
             {
                 ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
-                // calculate something for us to return
-                int length = (activity.Text ?? string.Empty).Length;
+                Activity reply;
 
-                // return our reply to the user
-                Activity reply = activity.CreateReply($"You sent {activity.Text} which was {length} characters");
+                if (Contain("badwords", activity.Text))
+                {
+                    reply = activity.CreateReply(Random("badwordsanswers"));
+                }
+                else if (Contain("greetings", activity.Text))
+                {
+                    reply = activity.CreateReply("Olá! Eu sou o Super Claudio. Estou aqui para lhe guiar na nuvem. Vamos começar? Também posso traduzir algum serviço da aws para o Azure, caso deseje.");
+                }
+                else if (Contain("goodbyes", activity.Text))
+                {
+                    reply = activity.CreateReply(Random("goodbyesanswers"));
+                }
+                else
+                {
+                    reply = activity.CreateReply(Random("missunderstood"));
+                }
+
                 await connector.Conversations.ReplyToActivityAsync(reply);
             }
             else
@@ -64,6 +82,33 @@ namespace superbotcloudV3
             }
 
             return null;
+        }
+
+        private bool Contain(string filename, string msg)
+        {
+            string path = "~/Docs/" + filename + ".json";
+            var json = File.ReadAllText(HttpContext.Current.Server.MapPath(path));
+
+            dynamic array = JsonConvert.DeserializeObject(json);
+            foreach (string word in array.data)
+            {
+                if (msg.ToLower().Contains(word.ToLower())) return true;
+            }
+            return false;
+        }
+
+        private string Random(string filename)
+        {
+            string path = "~/Docs/" + filename + ".json";
+            var json = File.ReadAllText(HttpContext.Current.Server.MapPath(path));
+
+            dynamic array = JsonConvert.DeserializeObject(json);
+            List<string> x = new List<string>();
+            foreach (string word in array.data)
+            {
+                x.Add(word);
+            }
+            return x[random.Next(x.Count())];
         }
     }
 }
