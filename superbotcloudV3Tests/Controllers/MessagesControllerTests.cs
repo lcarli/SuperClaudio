61d142e56f657e9c6320e89fee964a0295c38a03
Lucas <lucas.decarli@gmail.com>
2016-07-29 18:12:12 -0700

    Teste unitário #2

---
+++ superbotcloudV3Tests/Controllers/MessagesControllerTests.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using superbotcloudV3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace superbotcloudV3.Tests
+{
+    [TestClass()]
+    public class MessagesControllerTests
+    {
+        [TestMethod()]
+        public void PostTest()
+        {
+            Assert.IsNotNull("");
+        }
+        [TestMethod()]
+        public void PostTestMsg()
+        {
+            Assert.IsNotNull("");
+        }
+        [TestMethod()]
+        public void PostTestAPI()
+        {
+            Assert.IsNotNull("");
+        }
+        [TestMethod()]
+        public void PostTestGetLinks()
+        {
+            Assert.IsNotNull("");
+        }
+        [TestMethod()]
+        public void PostTestCrawler()
+        {
+            Assert.IsNotNull("");
+        }
+        [TestMethod()]
+        public void PostTestUpdateJSON()
+        {
+            Assert.IsNotNull("");
+        }
+        [TestMethod()]
+        public void PostTestAWS()
+        {
+            Assert.IsNotNull("");
+        }
+        [TestMethod()]
+        public void PostTestLanguage()
+        {
+            Assert.IsNotNull("");
+        }
+        [TestMethod()]
+        public void PostTestLocalLinks()
+        {
+            Assert.IsNotNull("");
+        }
+    }
+}
